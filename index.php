eb752693306aa8029312897d6b2e015ed374d88e
John Doe <johndoe@example.com>
2023-04-21 17:27:58 -0400

    hola

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Instagram login Form</title>
+    <!-- External css -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Font awesome link -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" >
+</head>
+<body>
+    <div id="wrapper">
+    	<div class="container">
+    		<div class="phone-app-demo"></div>
+    		<div class="form-data">
+    			<form action="">
+    				<div class="logo">
+    					<h1><i data-visualcompletion="css-img" aria-label="Instagram" class="" role="img" style="background-image: url(&quot;https://static.cdninstagram.com/rsrc.php/v3/yS/r/ajlEU-wEDyo.png&quot;); background-position: 0px -52px; background-size: auto; width: 175px; height: 51px; background-repeat: no-repeat; display: inline-block;"></i></h1>
+    				</div>
+    				<input type="text" placeholder="Phone number, username, or email">
+    				<input type="text" placeholder="Password">
+    				<button class="form-btn" type="submit">Log in</button>
+    				<span class="has-separator">Or</span>
+    				<a href="#" class="facebook-login">
+    					<i class="fab fa-facebook"></i> Log in with Facebook
+    				</a>
+    				<a class="password-reset" href="#">Forgot password?</a>
+    			</form>
+    			<div class="sign-up">
+    				Don't an account? <a href="#">Sign up</a>
+    			</div>
+    			<div class="get-the-app">
+    				<span>Get the app</span>
+    				<div class="badge">
+    					<img alt="Descargar en Google Play" class="_aa5q" src="https://static.cdninstagram.com/rsrc.php/v3/yT/r/0BpxdgM3WUr.png">
+    					<img alt="Descargar desde Microsoft" class="_aa5q" src="https://static.cdninstagram.com/rsrc.php/v3/y9/r/LjIQEL8csb0.png">
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+
+    	<footer>
+    		<div class="container">
+    			<nav class="footer-nav">
+    				<ul>
+    					<li><a href="#">About Us</a></li>
+    					<li><a href="#">Support</a></li>
+    					<li><a href="#">Jobs</a></li>
+    					<li><a href="#">Privacy</a></li>
+    					<li><a href="#">Terms</a></li>
+    					<li><a href="#">Profiles</a></li>
+    					<li><a href="#">Languages</a></li>
+    				</ul>
+    			</nav>
+    			<div class="copyright-notice">
+    				&copy; 2019 Complaints
+    			</div>
+    		</div>
+    	</footer>
+    </div>
+</body>
+</html>
